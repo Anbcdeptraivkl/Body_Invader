c71644a0c67244a378f1b0588aefc108ff8ca068
Anbcdeptraivkl <hothaian911@gmail.com>
2019-03-15 16:25:12 +0700

    Implemented the Restart Function
    Rename the Scenes to Level names. Fixed some naming errors. Implemented the Scene Restart Function: activate on Restart Button click (the script 'Restarting' are nome composed into the GameController object).

---
+++ Assets/Scripts/Restarting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restarting : MonoBehaviour
+{
+    //Restart the Scene / Level:
+    public void OnScenceRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
