b0d5751a49271400ff293d0bd8e54d2f92d3ce61
Anbcdeptraivkl <hothaian911@gmail.com>
2019-03-17 10:55:53 +0700

    Functioning Menu
    Main Menu Buttons now have real functionalities! Add the 'return to main menu functionality'.

---
+++ Assets/Scripts/MainMenuFunctionalController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunctionalController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //Load the next (Level 0) scene:
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+}
