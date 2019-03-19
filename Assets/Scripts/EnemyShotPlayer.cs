7703e395dcce193fcdf2f87aa5524b4488aa19b6
Anbcdeptraivkl <hothaian911@gmail.com>
2019-03-19 21:13:22 +0700

    Implemented Enemy Shots
    Enemies now can shot to destroy player. Implemented SFX on enemy's explosions and reaction of player when got shot by enemies.

---
+++ Assets/Scripts/EnemyShotPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*When the enemy's bolt hit the player, esplode */
+public class EnemyShotPlayer : MonoBehaviour
+{
+    public GameObject playerExplosion;
+    //Script References:
+	private GameController gameController;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if (gameControllerObject != null) 
+		{
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+			else
+			{
+				Debug.Log("Failed to load <GameController> script component.");
+			}
+	}
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerExplosion = Instantiate(
+                playerExplosion, 
+                other.transform.position,
+                other.transform.rotation) as GameObject;
+            Destroy(other.gameObject);
+            Destroy(playerExplosion, 2.0f);
+            Destroy(gameObject);
+            //Game over since the player has been destroyed:
+            gameController.GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyWeapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Enemy SHooting projectiles overtime */
+public class EnemyWeapon : MonoBehaviour
+{
+    public GameObject enemyShot;
+    public Transform shotSpawn;
+    public float delayTime;
+    public float fireRate;
+
+    void Start()
+    {
+        InvokeRepeating("Fire", delayTime, fireRate);
+    }
+
+    private void Fire()
+    {
+        Instantiate(enemyShot, shotSpawn.position, shotSpawn.rotation);
+    }
+}
