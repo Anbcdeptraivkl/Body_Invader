c04708a86889d6d51fb373311b8b2090aa346660
Anbcdeptraivkl <hothaian911@gmail.com>
2019-04-03 10:01:47 +0700

    Implementing Volume Settings

---
+++ Assets/Scripts/OptionSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionSettings : MonoBehaviour
+{
+   public AudioMixer audioMixer;
+
+   public void VolumeSLiding (float volume) {
+       audioMixer.SetFloat("masterVolume", volume);
+   }
+}
